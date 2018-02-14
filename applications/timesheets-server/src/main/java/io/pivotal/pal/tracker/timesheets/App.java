fd3199b5f191809e290a4bb0c4d735cfcbd304db
Sarah Niemeyer <sniemeyer@pivotal.io>
2018-02-14 15:17:13 -0700

    Add eureka server so Turbine knows where instances are

--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
@@ -13,6 +14,7 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableDiscoveryClient
 @ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
 public class App {
 
